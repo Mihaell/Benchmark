81bae8e32995222adcfcd121fcf367956475618d
Mihael Liskij <mihael.liskij@gmail.com>
2014-06-12 20:34:48 +0200

    Napravio pregled igrice

--- games.py
+++ games.py
@@ -2,5 +2,51 @@ import utils
 import globals
 
 def choose_game( x ):
-  print ( '' )
+  utils.clear_screen()
+  print ( globals.igrice[x] )
 
+  igra = globals.igrice[x]
+
+  print ( '==+' + '='*100 + '+==' )
+  print ( '  |' + ' '*100 + '|  ' )
+  print ( '  |' + igra['ime'].center( 100, ' ' ) + '|  ' )
+  print ( '  |' + ' '*100 + '|  ' )
+  print ( '  |' + ('*'*int(igra['ocjena'])).center( 100, ' ' ) + '|  ' )
+  print ( '  |' + ' '*100 + '|  ' )
+  print ( '==+' + '='*20 + '+' + '='*79 + '+==' )
+  print ( '  |' + ' '*20 + '|' + ' '*79 + '|  ' )
+  print ( '  |' + 'ZANR'.center( 20, ' ' ) + '|' + igra['zanr'].center( 79, ' ' ) + '|  ' )
+  print ( '  |' + ' '*20 + '|' + ' '*79 + '|  ' )
+  print ( '==+' + '-'*20 + '+' + '-'*79 + '+==' )
+  print ( '  |' + ' '*20 + '|' + ' '*79 + '|  ' )
+  print ( '  |' + 'MINIMALNI CPU'.center( 20, ' ' ) + '|' + str(igra['min_cpu']).center( 79, ' ' ) + '|  ' )
+  print ( '  |' + ' '*20 + '|' + ' '*79 + '|  ' )
+  print ( '==+' + '-'*20 + '+' + '-'*79 + '+==' )
+  print ( '  |' + ' '*20 + '|' + ' '*79 + '|  ' )
+  print ( '  |' + 'MINIMALNI GPU'.center( 20, ' ' ) + '|' + str(igra['min_gra_cpu']).center( 79, ' ' ) + '|  ' )
+  print ( '  |' + ' '*20 + '|' + ' '*79 + '|  ' )
+  print ( '==+' + '-'*20 + '+' + '-'*79 + '+==' )
+  print ( '  |' + ' '*20 + '|' + ' '*79 + '|  ' )
+  print ( '  |' + 'MINIMALNI GPU_RAM'.center( 20, ' ' ) + '|' + str(igra['min_gra_ram']).center( 79, ' ' ) + '|  ' )
+  print ( '  |' + ' '*20 + '|' + ' '*79 + '|  ' )
+  print ( '==+' + '-'*20 + '+' + '-'*79 + '+==' )
+  print ( '  |' + ' '*20 + '|' + ' '*79 + '|  ' )
+  print ( '  |' + 'MINIMALNI RAM'.center( 20, ' ' ) + '|' + str(igra['min_ram']).center( 79, ' ' ) + '|  ' )
+  print ( '  |' + ' '*20 + '|' + ' '*79 + '|  ' )
+  print ( '==+' + '-'*20 + '+' + '-'*79 + '+==' )
+  print ( '  |' + ' '*100  + '|  ' )
+  print ( '  |' + ('Cijena: ' + str(igra['cijena']) + ' kn').center( 100, ' ' ) + '|  ' )
+  print ( '  |' + ' '*100  + '|  ' )
+  print ( '==+' + '='*100 + '+==' )
+  print ( '' )
+  print ( '        1) Benchmark')
+  print ( '        0) Izlaz')
+
+  choice = 0;
+  int( input( choice ) )
+
+  if choice == 0:
+    return False
+  if choice == 1:
+    print("TADAAAAA")
+  input(x)
